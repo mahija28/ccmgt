534cb7acfefd647617ebfa355ac0802ab60dbe89
Mahija Daliparthi <dmahija@pdx.edu>
2016-03-12 16:27:02 -0800

    Corrected print statements.

--- src/main/java/edu/pdx/ccmgt/ccmgt/Query1.java
+++ src/main/java/edu/pdx/ccmgt/ccmgt/Query1.java
@@ -20,30 +20,34 @@ public class Query1 extends Query{
 		System.out.println("Detector Id's When Speed > 100:");
 		
         //Executing the query
-        result1 = session.execute("select detectorid, count(detectorid) as count from QOEdit1 where dqflags = 4");
-        result2 = session.execute("select detectorid, count(detectorid) as count from QOEdit1 where dqflags = 5");
-        result3 = session.execute("select detectorid, count(detectorid) as count from QOEdit1 where dqflags = 6");
-        result4 = session.execute("select detectorid, count(detectorid) as count from QOEdit1 where dqflags = 7");
+        result1 = session.execute("select count(*) as count from QOEdit1 where dqflags = 4");
+        result2 = session.execute("select count(*) as count from QOEdit1 where dqflags = 5");
+        result3 = session.execute("select count(*) as count from QOEdit1 where dqflags = 6");
+        result4 = session.execute("select count(*) as count from QOEdit1 where dqflags = 7");
         
         for(Row row: result1){
-        	System.out.format("%d %d\n", row.getInt("detectorid"), row.getLong("count"));
+        	System.out.println("When dqflags=4");
+        	System.out.format("%d\n", row.getLong("count"));
         	dqflagFour = row.getLong("count");
-        	//System.out.format("%d\n", row.getLong("count"));
+        	
         }
         for(Row row:result2){
-        	System.out.format("%d\n", row.getInt("detectorid"), row.getLong("count"));
+        	System.out.println("when dqflags=5");
+        	System.out.format("%d\n", row.getLong("count"));
         	 dqflagFive = row.getLong("count");
-        	//System.out.format("%d\n", row.getLong("count"));
+        	
         }
         for(Row row:result3){
-        	System.out.format("%d\n", row.getInt("detectorid"), row.getLong("count"));
+        	System.out.println("when dqflags=6");
+        	System.out.format("%d\n", row.getLong("count"));
         	 dqflagSix = row.getLong("count");
-        	//System.out.format("%d\n", row.getLong("count"));
+        	
         }
         for(Row row:result4){
-        	System.out.format("%d\n", row.getInt("detectorid"), row.getLong("count"));
+        	System.out.println("when dqflags=7");
+        	System.out.format("%d\n", row.getLong("count"));
         	 dqflagSeven = row.getLong("count");
-        	//System.out.format("%d\n", row.getLong("count"));
+        
         }
         
         Long finalResult = dqflagFour + dqflagFive + dqflagSix + dqflagSeven;
